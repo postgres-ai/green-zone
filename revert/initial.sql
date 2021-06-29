d2cd0368281b850eff1be417af19c4c2238d66b0
akartasov <agneum@gmail.com>
2021-06-29 13:57:55 +0700

    initial schema

---
+++ revert/initial.sql
@@ -0,0 +1,7 @@
+-- Revert green-zone:initial from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ verify/initial.sql
@@ -0,0 +1,7 @@
+-- Verify green-zone:initial on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
