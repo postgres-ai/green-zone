b696bf5cd75188eb0f9c21bd3f2297a424547d7e
akartasov <agneum@gmail.com>
2021-07-08 15:32:33 +0700

    create a new index concurrently outside a transaction block

--- deploy/pgbench-accounts-index-bid.sql
+++ deploy/pgbench-accounts-index-bid.sql
@@ -1,7 +1,3 @@
 -- Deploy green-zone:pgbench-accounts-index-bid to pg
 
-begin;
-
 create index concurrently bid_idx on pgbench_accounts(bid);
-
-commit;
--- revert/pgbench-accounts-index-bid.sql
+++ revert/pgbench-accounts-index-bid.sql
@@ -1,7 +1,3 @@
 -- Revert green-zone:pgbench-accounts-index-bid from pg
 
-begin;
-
 drop index concurrently if exists bid_idx;
-
-commit;
